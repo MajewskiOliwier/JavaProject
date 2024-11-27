159713441fd1dbc924544ee1ede2106991c5de08
Tomasz Marek <tomasz.marek02@gmail.com>
2024-11-27 12:12:46 +0100

    import recipes from json/xml work

--- src/main/java/com/example/JavaProject/service/interfaces/ImportService.java
+++ src/main/java/com/example/JavaProject/service/interfaces/ImportService.java
@@ -1,16 +1,11 @@
 package com.example.JavaProject.service.interfaces;
 
-import com.example.JavaProject.dto.RecipeDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 public interface ImportService {
 
-    List<RecipeDto> importRecipesFromJson(String json) throws IOException;
-
-//    List<RecipeDto> importRecipesFromJson(MultipartFile json) throws IOException;
-
-    List<RecipeDto> importRecipesFromXml(String xml) throws IOException;
+    void importRecipesFromJson(MultipartFile file) throws IOException;
+    void importRecipesFromXml(MultipartFile file) throws IOException;
 }
