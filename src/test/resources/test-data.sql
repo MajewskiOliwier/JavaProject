80f553a7925819447eb25af1e3926db36ba1d98a
Tomasz Marek <tomasz.marek02@gmail.com>
2024-12-04 18:23:18 +0100

    RepairOldTestsCreatedNewTests

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,5 +1,10 @@
-delete from users;
-insert into role (name) values ('ROLE_USER');
+
+DELETE FROM users;
+DELETE FROM role;
+
+INSERT INTO role (id, name) VALUES (1, 'ROLE_USER'), (2, 'ROLE_ADMIN');
 
 INSERT INTO users (user_name, age, is_man, email, password, role_id, is_hidden, created_at, updated_at)
-VALUES ('username', 30, TRUE, 'email@email.com', '$2a$12$lpwBT6UMxH8qkdanUpHxXuenpbmb3b3ic/BcILqWcJUGRbku2OWay', 1, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES
+    ('username', 30, TRUE, 'email@email.com', '$2a$12$lpwBT6UMxH8qkdanUpHxXuenpbmb3b3ic/BcILqWcJUGRbku2OWay', 1, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('admin', 40, TRUE, 'admin@email.com', '$2a$12$lpwBT6UMxH8qkdanUpHxXuenpbmb3b3ic/BcILqWcJUGRbku2OWay', 2, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
